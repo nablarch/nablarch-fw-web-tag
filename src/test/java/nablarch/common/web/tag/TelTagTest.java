a07a2c4b05e6acdfe217abd97430ee4abd25cad2
kiyohome <kiyohito.itoh@gmail.com>
2021-03-03 17:18:05 +0900

    telタグを追加。

---
+++ src/test/java/nablarch/common/web/tag/TelTagTest.java
@@ -0,0 +1,10 @@
+package nablarch.common.web.tag;
+
+/**
+ * @author Kiyohito Itoh
+ */
+public class TelTagTest extends TextTagTestSupport<TelTag> {
+    public TelTagTest() {
+        super(new TelTag(), "tel");
+    }
+}
