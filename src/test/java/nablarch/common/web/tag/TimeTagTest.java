e36b4bf33b073788ef1c09516fd5f8141dca27c9
kiyohome <kiyohito.itoh@gmail.com>
2021-03-03 23:37:37 +0900

    timeタグを追加。

---
+++ src/test/java/nablarch/common/web/tag/TimeTagTest.java
@@ -0,0 +1,10 @@
+package nablarch.common.web.tag;
+
+/**
+ * @author Kiyohito Itoh
+ */
+public class TimeTagTest extends TextTagTestSupport<TimeTag> {
+    public TimeTagTest() {
+        super(new TimeTag(), "time");
+    }
+}
