fc728cfd97a3b04124114f663304a932fa2a78f6
kiyohome <kiyohito.itoh@gmail.com>
2021-03-03 23:25:28 +0900

    weekタグを追加。

---
+++ src/test/java/nablarch/common/web/tag/WeekTagTest.java
@@ -0,0 +1,10 @@
+package nablarch.common.web.tag;
+
+/**
+ * @author Kiyohito Itoh
+ */
+public class WeekTagTest extends TextTagTestSupport<WeekTag> {
+    public WeekTagTest() {
+        super(new WeekTag(), "week");
+    }
+}
