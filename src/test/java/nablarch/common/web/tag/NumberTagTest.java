fedb94d1a5b9245d5b23d4def3456b8886c5135a
kiyohome <kiyohito.itoh@gmail.com>
2021-03-03 23:56:16 +0900

    numberタグを追加。

---
+++ src/test/java/nablarch/common/web/tag/NumberTagTest.java
@@ -0,0 +1,10 @@
+package nablarch.common.web.tag;
+
+/**
+ * @author Kiyohito Itoh
+ */
+public class NumberTagTest extends TextTagTestSupport {
+    public NumberTagTest() {
+        super(new NumberTag(), "number");
+    }
+}
