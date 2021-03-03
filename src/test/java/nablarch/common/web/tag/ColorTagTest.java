b042aa742a1da265528e3f4b2568a17dcf4583e4
kiyohome <kiyohito.itoh@gmail.com>
2021-03-04 00:21:49 +0900

    colorタグを追加。

---
+++ src/test/java/nablarch/common/web/tag/ColorTagTest.java
@@ -0,0 +1,10 @@
+package nablarch.common.web.tag;
+
+/**
+ * @author Kiyohito Itoh
+ */
+public class ColorTagTest extends TextTagTestSupport<ColorTag> {
+    public ColorTagTest() {
+        super(new ColorTag(), "color");
+    }
+}
